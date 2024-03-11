203e3dcbc0f5b41ab19560fa9e629e66d58624b1
alexrojas431 <alexrojas431@gmail.com>
2024-03-10 19:44:53 -0500

    - Added descriptions to remaining projects

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -21,19 +21,39 @@ export default function Home() {
           <h2>
             Star Wars API <span>-&gt;</span>
           </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
+          <p>
+            My friend really likes board games and wanted to experiment with
+            translating a Star Wars themed board game into a virtual medium. One
+            step he decided to take was to make an API so the game can have all
+            the information readily available to it. This was a big project, so
+            he asked for my help.
+          </p>
         </article>
         <article className={styles.card}>
           <h2>
             ReactJam Winter 23/24: Cyberpunk Tycoon <span>-&gt;</span>
           </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+          <p>
+            Along with a friend of mine, we used React.JS to make a game within
+            12 days in the event called ReactJam 23/24. One thing you'll notice
+            is that it has little to it. I prefer the work we did on the project
+            rather than the result. It was our first game jam and our first time
+            making a video game. Due to this, we needed to learn a lot very
+            quickly. The biggest thing was utilizing Graphic resources as best
+            as possible.
+          </p>
         </article>
         <article className={styles.card}>
           <h2>
             Rollback Netcode Implementation <span>-&gt;</span>
           </h2>
-          <p>Explore starter templates for Next.js.</p>
+          <p>
+            One of the reasons why I decided to pursue a career in programming
+            is because of my interest in making videogames. While I'm not as
+            interested in a career within the videogame industry, I still love
+            videogames as a hobby and this personal project is just another
+            example of that.
+          </p>
         </article>
       </div>
     </main>
