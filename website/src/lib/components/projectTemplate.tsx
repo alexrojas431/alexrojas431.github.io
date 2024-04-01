c425136e68a6a7ae6993847fe2c9b28080375db8
alexrojas431 <alexrojas431@gmail.com>
2024-04-01 17:28:07 -0500

    projectTemplate bug fix and projectCSS changes
    projectTemplate
    - Used splitContent function to add new lines.
    - splitContent id paramater should have special diferentiator added to it as a standard. This was done here.
    - Created isFinalItem function to determine which list item is the last one and needs special CSS applied to it
    
    starwarsCSS
    - Removed css that could be generally applied to all projects
    
    projectCSS
    - Added more padding to boxes for a better look

--- website/src/lib/components/projectTemplate.tsx
+++ website/src/lib/components/projectTemplate.tsx
@@ -4,6 +4,7 @@ import {
   ProjectTemplateInterface,
   TechStack,
 } from "@/lib/interface/projectTemplateInterface";
+import { splitContent } from "@/util/splitContent";
 import styles from "@/scss/projects.module.scss";
 
 interface Props {
@@ -11,6 +12,29 @@ interface Props {
 }
 
 const ProjectTemplate = (p: Props) => {
+  /**
+   * isFinalItem
+   *
+   * @what
+   * Determines if the index given is the last element in the array
+   * and then if the index is odd
+   *
+   * @why
+   * CSS style finalItem should only apply to the last list item
+   * and only if that item needs to take space in both columns (odd index)
+   *
+   * @param i - current index of array (starts at 0)
+   * @param sizeOfArray - full length of array
+   * @returns boolean
+   */
+
+  const isFinalItem = (i: number, sizeOfArray: number): boolean => {
+    if (i === sizeOfArray - 1) {
+      return i % 2 === 0;
+    }
+    return false;
+  };
+
   return (
     <Fragment>
       {p.projectPageData.map((project: ProjectTemplateInterface) => (
@@ -21,8 +45,15 @@ const ProjectTemplate = (p: Props) => {
             </header>
             <br />
             <ul className={styles.gridList}>
-              {project.summaryList.map((point: ListPoint) => (
-                <li key={point.id}>
+              {project.summaryList.map((point: ListPoint, index: number) => (
+                <li
+                  key={point.id}
+                  className={`${
+                    isFinalItem(index, project.summaryList.length)
+                      ? styles.finalItem
+                      : ""
+                  }`}
+                >
                   <h3>{point.title}</h3>
                   {point.content}
                 </li>
@@ -34,7 +65,7 @@ const ProjectTemplate = (p: Props) => {
             <header>
               <h2>Tech Stack</h2>
             </header>
-            <p>{project.techStack}</p>
+            <p>{splitContent(project.id + "-techStack", project.techStack)}</p>
             <section>
               <br />
               <b>Tech Used</b>
@@ -49,14 +80,14 @@ const ProjectTemplate = (p: Props) => {
             <header>
               <h2>Architecture</h2>
             </header>
-            <p>{project.architecture}</p>
+            <p>{splitContent(project.id + "-arch", project.architecture)}</p>
           </section>
           <br />
           <section>
             <header>
               <h2>Overall Description</h2>
             </header>
-            <p>{project.description}</p>
+            <p>{splitContent(project.id + "-desc", project.description)}</p>
           </section>
         </Fragment>
       ))}
