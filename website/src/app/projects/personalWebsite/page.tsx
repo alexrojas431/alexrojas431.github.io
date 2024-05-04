5feafb1fc8dabd1317d55563606d99eb63abff29
alexrojas431 <alexrojas431@gmail.com>
2024-05-03 23:16:23 -0500

    Created CSS for personal website
    projects css
    - Halved the size of the border for a better appeareance
    
    personalSite tsx css
    - Created relevant tags for css
    - Added span tag to hold the background
    - Added image for background

--- website/src/app/projects/personalWebsite/page.tsx
+++ website/src/app/projects/personalWebsite/page.tsx
@@ -4,19 +4,21 @@ import ProjectTemplate from "@/components/projectTemplate";
 import { personalsiteProjectTemplate } from "@/data/projectTemplateData/personalsiteProjectTemplate";
 import { personalSiteLogoData } from "@/data/logoData";
 import styles from "@/scss/projects.module.scss";
+import web from "@/scss/pages/personalSite.module.scss";
 
 export default function PersonalWebsite() {
   return (
     <main className={styles.main}>
+      <span id={web.webMain}></span>
       <Link href="/">
-        <h3 className={styles.arrow}>
+        <h3 className={styles.arrow} id={web.webArrow}>
           <span>&lt;-</span> Go Back to home page
         </h3>
       </Link>
-      <article className={styles.card}>
+      <article className={styles.card} id={web.webCard}>
         <header>
           <h1>My personal website</h1>
-          <div className={styles.grid}>
+          <div className={styles.grid} id={web.webGrid}>
             <LogoRow logoData={personalSiteLogoData} />
           </div>
         </header>
