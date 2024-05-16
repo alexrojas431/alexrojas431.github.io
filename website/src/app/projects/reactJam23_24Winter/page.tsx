5cb801ff9fcf4d32ee55fd09bb1d52c0a6e81bbf
alexrojas431 <alexrojas431@gmail.com>
2024-05-16 18:44:19 -0500

    Added css for reactJam winter project page, changed css in root and personalSite
    - Added images for background and color gradient
    
    - Needs to add css for arrow
    
    Root css
    - Removed dimensions from html and moved to body as it may help avoid future problems
    - Changed % to vh for more concrete units
    
    personalSite
    - Changed box-shadow to not use rgba as it was not neccessary

--- website/src/app/projects/reactJam23_24Winter/page.tsx
+++ website/src/app/projects/reactJam23_24Winter/page.tsx
@@ -10,13 +10,13 @@ import jam from "@/scss/pages/reactJam23_24Winter.module.scss";
 
 export default function ReactJam23_24Winter() {
   return (
-    <main className={styles.main}>
+    <main className={styles.main} id={jam.jam23WinterMain}>
       <Link href="/">
         <h3 className={styles.arrow} id={jam.jam23WinterArrow}>
           <span>&lt;-</span> Go Back to home page
         </h3>
       </Link>
-      <article className={styles.card}>
+      <article className={styles.card} id={jam.jam23WinterCard}>
         <header>
           <h1>Cyberpunk Tycoon</h1>
           <div className={styles.grid} id={jam.jam23WinterGrid}>
@@ -27,6 +27,11 @@ export default function ReactJam23_24Winter() {
         <ProjectTemplate
           projectTemplateData={reactJam23_24WinterProjectTemplate}
         />
+        snowflake image credits:
+        <br />
+        <a href="https://www.freepik.com/free-vector/realistic-falling-white-snow-overlay-transparent-background-snowflakes-storm-layer_19393340.htm#fromView=search&page=1&position=0&uuid=4bb644e0-4459-4563-a159-391cc377b1be">
+          Image by svstudioart on Freepik
+        </a>
       </article>
     </main>
   );
