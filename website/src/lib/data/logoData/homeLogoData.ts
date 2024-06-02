f88497c32e5ca092781f7fbe1ed6d91524700e59
alexrojas431 <alexrojas431@gmail.com>
2024-06-02 14:14:23 -0500

    Added better LinkedIn icons, added to logoRow
    - The new icons are bigger, also brought in white version for better contrast
    
    starwars and home logoData
    - Removed '-1' from image id as it was unecessary
    
    starwars logoData
    - Added link to Ryan Llamas linkedIn profile
    
    starwars logoRow scss
    - SCSS is altered to accomodate added logo in logoRow

--- website/src/lib/data/logoData/homeLogoData.ts
+++ website/src/lib/data/logoData/homeLogoData.ts
@@ -1,13 +1,13 @@
 import { LogoInterface } from "@/interface/logoInterface";
 import GitHubIcon from "@/public/github-mark-white.svg";
-import LinkedInIcon from "@/public/In-Blue-128.png";
+import LinkedInIcon from "@/public/In-Blue-128@2x.png";
 
 export const homeLogoData: LogoInterface[] = [
   {
     id: "homeLogo-1",
     link: "https://github.com/alexrojas431",
     image: {
-      id: "homeLogo-1-img-1",
+      id: "homeLogo-1-img",
       src: GitHubIcon,
       alt: "GitHub Icon, click to go to my GitHub page",
     },
@@ -17,7 +17,7 @@ export const homeLogoData: LogoInterface[] = [
     id: "homeLogo-2",
     link: "https://www.linkedin.com/in/alexanderrojas431/",
     image: {
-      id: "homeLogo-2-img-1",
+      id: "homeLogo-2-img",
       src: LinkedInIcon,
       alt: "LinkedIn Icon, click to go to my LinkedIn page",
     },
--- website/src/lib/data/logoData/starWarsLogoData.ts
+++ website/src/lib/data/logoData/starWarsLogoData.ts
@@ -1,6 +1,7 @@
 import { LogoInterface } from "@/interface/logoInterface";
 import GitHubIcon from "@/public/github-mark-white.svg";
 import APIArch from "@/public/StarWars/StarWarsCompAPI-Architecture.png";
+import LinkedInIcon from "@/public/In-White-128@2x.png";
 import wikiIcon from "@/public/wiki icon.png";
 
 export const starLogoData: LogoInterface[] = [
@@ -8,7 +9,7 @@ export const starLogoData: LogoInterface[] = [
     id: "starLogo-1",
     link: "https://github.com/LlamasOnTheRun/Star-Wars-Rebellion-Component-Info-API",
     image: {
-      id: "starLogo-1-img-1",
+      id: "starLogo-1-img",
       src: GitHubIcon,
       alt: "GitHub Icon, click to go to the projects GitHub Page",
     },
@@ -18,7 +19,7 @@ export const starLogoData: LogoInterface[] = [
     id: "starLogo-2",
     link: "https://github.com/LlamasOnTheRun/Star-Wars-Rebellion-Component-Info-API/wiki",
     image: {
-      id: "starLogo-2-img-1",
+      id: "starLogo-2-img",
       src: wikiIcon,
       alt: "Wiki icon, click to go to the projects wiki",
       credits: "Wiki icons created by Freepik - Flaticon",
@@ -31,10 +32,20 @@ export const starLogoData: LogoInterface[] = [
     id: "starLogo-3",
     link: "https://github.com/LlamasOnTheRun/Star-Wars-Rebellion-Component-Info-API/wiki/Architecture-Overview",
     image: {
-      id: "starLogo-3-img-1",
+      id: "starLogo-3-img",
       src: APIArch,
       alt: "Picture of architecture of API, click to go to the architecture section of the wiki",
     },
     description: "Detailed explanation of architecture of API",
+  },
+  {
+    id: "starLogo-4",
+    link: "https://www.linkedin.com/in/ryan-llamas-05459414a/",
+    image: {
+      id: "starLogo-4-img",
+      src: LinkedInIcon,
+      alt: "LinkedIn icon, click to go to the LinkedIn profile",
+    },
+    description: "Ryan Llamas started the project, check out his other work.",
   },
 ];
