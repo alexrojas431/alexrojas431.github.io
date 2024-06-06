791c71bded7e73bd132d4be6633c13c72e70afa8
alexrojas431 <alexrojas431@gmail.com>
2024-06-06 17:23:40 -0500

    Added TODO, Revise Home page contents
    New TODO
    - Further site optimizations, this specific to project pages possible layout file

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -4,6 +4,7 @@ import "@/scss/layout/root.scss";
 //TODO: Add error boundary to handle custom errors made
 //TODO: Look into adding CDN, Content Delivery Network to store images. or find a way to decrease image size for sake of 1GB limit on GitHub
 //TODO: Optimize the site through composition like child props or comp props. Also React.memo if needed.
+//TODO: Like the TODO above, make layout of projects use child props
 
 const inter = Inter({ subsets: ["latin"] });
 
