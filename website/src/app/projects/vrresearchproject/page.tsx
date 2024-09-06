ea837ffa35488896849ed3a3419cde707c3b4965
alexrojas431 <alexrojas431@gmail.com>
2024-09-05 20:05:45 -0500

    Separated intro into its own section in the project page

--- website/src/app/projects/vrresearchproject/page.tsx
+++ website/src/app/projects/vrresearchproject/page.tsx
@@ -22,6 +22,33 @@ export default function VRresearchProject() {
             <LogoRow logoData={vrResearchProjectLogoData} />
           </div>
         </header>
+        <section>
+          <header>
+            <h2>Introduction</h2>
+          </header>
+          <p>
+            I was invited into the research project by my then classmate Ivan
+            Sanchez, the project was supervised and supported by Dr. Papka and
+            Dr. Zachary Wahl-Alexander. By the time I joined the project, a
+            pilot of the experiment had been conducted and a 3D model partially
+            made, I would be conducting the full experiment and making
+            adjustments to the model. In short, we would be performing the
+            experiment using that model and writing a research paper explaining
+            the process and our conclusions. All in the effort to see if virtual
+            reality (VR) could be used in an educational setting, specifically
+            to prepare aspiring teachers through VR.
+            <br />
+            <br />
+            This page will be an overview of the project but you can check out
+            the research paper linked above for more details. This page will
+            also primarily talk about my contributions to the project,
+            that&apos;s not to say that I was the only one doing the work. The
+            people I previously mentioned contributed a lot, I have links to
+            their LinkedIn page above. This project is largely unrelated to my
+            software engineering skills so undertaking it was very new to me but
+            even still it allowed me to improve my skills in other areas.
+          </p>
+        </section>
         <ProjectTemplate projectTemplateData={vrResearchProjectTemplate} />
       </article>
     </main>
